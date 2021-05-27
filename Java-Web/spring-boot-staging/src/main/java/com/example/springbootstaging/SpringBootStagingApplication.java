26f494fdc5df9747a8b005bf253943fb3dbe8de4
huangzongzhen <huangzongzhuan@gmail.com>
2021-05-27 11:24:15 +0800

    update

---
+++ Java-Web/spring-boot-staging/src/main/java/com/example/springbootstaging/SpringBootStagingApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootstaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStagingApplication.class, args);
+	}
+
+}
---
+++ Java-Web/spring-boot-staging/src/test/java/com/example/springbootstaging/SpringBootStagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootstaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
