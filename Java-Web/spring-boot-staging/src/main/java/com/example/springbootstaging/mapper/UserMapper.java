8bab2af04652a33ce73138ac9a08030694c95c01
huangzongzhen <huangzongzhuan@gmail.com>
2021-06-17 11:29:30 +0800

    update

--- Java-Web/spring-boot-staging/src/main/java/com/example/springbootstaging/mapper/UserMapper.java
+++ Java-Web/spring-boot-staging/src/main/java/com/example/springbootstaging/mapper/UserMapper.java
@@ -9,8 +9,12 @@ import com.example.springbootstaging.model.User;
 @Repository
 public interface UserMapper {
     List<User> getALL();
+
     User getOne(Long id);
+
     void insert(User user);
+
     void update(User user);
+
     void delete(Long id);
 }
